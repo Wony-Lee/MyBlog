1a33943c3190ccd6616bd77e17700419c0f53bea
Wony-Lee <a1_11_11@naver.com>
2021-03-27 22:45:15 +0900

    CommentForm Update

--- client/components/CommentForm.js
+++ client/components/CommentForm.js
@@ -1,7 +1,7 @@
 import React, { useCallback } from "react";
 import styled from "@emotion/styled";
 import useInput from "../hooks/useInput";
-
+import PropTypes from "prop-types";
 const CommentLayout = styled.div`
   width: 100%;
   display: flex;
@@ -13,35 +13,44 @@ const CommentLayout = styled.div`
 
 const CommentsForm = styled.form`
   width: 100%;
+  display: flex;
+  align-items: center;
 `;
 
 const CommentInput = styled.input`
-  margin-left: 35px;
   border: 0px;
-  width: 90%;
+  width: 85%;
   height: 30px;
   outline: none;
+  margin-right: 12px;
 `;
 
 const CommentForm = ({ post }) => {
   const [comment, onChangeComment] = useInput("");
-  const onCommentSubmit = useCallback((e) => {
-    e.preventDefault();
-    console.log(e);
-  }, []);
+  const onCommentSubmit = useCallback(
+    (e) => {
+      e.preventDefault();
+      console.log(post.id, comment);
+    },
+    [comment]
+  );
 
   return (
     <>
       <CommentLayout>
         <CommentsForm onSubmit={onCommentSubmit}>
           <CommentInput value={comment} onChange={onChangeComment} />
+          <button type="submit" style={{ border: "1px solid aqua" }}>
+            작성
+          </button>
         </CommentsForm>
-        <button type="submit" style={{ border: "1px solid aqua" }}>
-          작성
-        </button>
       </CommentLayout>
     </>
   );
+};
+
+CommentForm.propTypes = {
+  post: PropTypes.object.isRequired,
 };
 
 export default CommentForm;
