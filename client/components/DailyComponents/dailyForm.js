bcb9a216a50a3d05bc96726ead333e65c2834831
Wony-Lee <a1_11_11@naver.com>
2021-04-17 18:43:56 +0900

    DailyForm Update

--- client/components/DailyComponents/dailyForm.js
+++ client/components/DailyComponents/dailyForm.js
@@ -1,15 +1,69 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-const DailyInput = styled.div`
+const Container = styled.div`
   border: 1px solid white;
   color: white;
+  margin-bottom: 30px;
+`;
+
+const PostForm = styled.form`
+  border: 1px solid white;
+`;
+
+const PostTitle = styled.div`
+  border: 1px solid white;
+  padding-top: 5px;
+  padding-bottom: 5px;
+  display: flex;
+  justify-content: center;
+`;
+const PostItem = styled.div`
+  border: 1px solid white;
+  display: flex;
+  justify-content: center;
+  padding-top: 5px;
+  padding-bottom: 5px;
+`;
+
+const PostInput = styled.input`
+  border: 0px;
+  outline: none;
+  height: 30px;
+  width: 50%;
+`;
+
+const PostTextarea = styled.textarea`
+  border: 0px;
+  resize: none;
+  outline: none;
+  width: 50%;
+  height: 200px;
+`;
+
+const PostBtnForm = styled.div`
+  border: 1px solid white;
+  display: flex;
+  justify-content: center;
 `;
 
 const DailyForm = () => {
   return (
     <>
-      <DailyInput>asdasd</DailyInput>
+      <Container>
+        <PostForm>
+          <PostTitle>
+            <PostInput placeholder="Title" />
+          </PostTitle>
+          <PostItem>
+            <PostTextarea></PostTextarea>
+          </PostItem>
+          <PostBtnForm>
+            <div>파일업로드버튼</div>
+            <div>업로드</div>
+          </PostBtnForm>
+        </PostForm>
+      </Container>
     </>
   );
 };
